09101ab17126b33c9e1a38a121b455418a7de137
matteobelladelli <matteobelladelli98@gmail.com>
2021-01-25 11:15:12 +0100

    .

---
+++ WeatherStation.ino
@@ -0,0 +1,12 @@
+#include <Arduino_FreeRTOS.h>
+#include <semphr.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
