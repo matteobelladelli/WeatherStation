2f96d840f0a99fffcf887643d5ca60633fab0ff1
matteobelladelli <matteobelladelli98@gmail.com>
2021-01-26 12:14:36 +0100

    references

--- WeatherStation.ino
+++ WeatherStation.ino
@@ -1,84 +1,27 @@
+/*
+ * Temperature
+ * Humidity
+ * Barometic Pressure
+ * Wind
+ * Rain
+ * UV Index
+ */
+
+
 #include <Arduino_FreeRTOS.h>
 #include <semphr.h>
 
-SemaphoreHandle_t xSerialSemaphore;
 
-TaskHandle_t TaskHandle_1;
-TaskHandle_t TaskHandle_2;
-TaskHandle_t TaskHandle_3;
-
-void TaskFunc_1(void *param);
-void TaskFunc_2(void *param);
-void TaskFunc_3(void *param);
-
-void setup() {
-  // put your setup code here, to run once:
-
+void setup()
+{
   Serial.begin(9600);
 
-  if (xSerialSemaphore == NULL)
-  {
-    xSerialSemaphore = xSemaphoreCreateMutex();
-    if ((xSerialSemaphore) != NULL)
-      xSemaphoreGive(xSerialSemaphore);
-  }
-
-  xTaskCreate(TaskFunc_1, "Task_1", 128, NULL, 1, &TaskHandle_1);
-  xTaskCreate(TaskFunc_2, "Task_2", 128, NULL, 2, &TaskHandle_2);
-  xTaskCreate(TaskFunc_3, "Task_3", 128, NULL, 3, &TaskHandle_3);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
-}
-
-void TaskFunc_1(void *param)
+void loop()
 {
-  (void) param;
 
 
-  for (;;)
-  {
-    if (xSemaphoreTake(xSerialSemaphore, (TickType_t) 5) == pdTRUE)
-    {
-      Serial.println("Task_1");
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
-    
-      xSemaphoreGive(xSerialSemaphore);
-    }
-  }
-}
-
-void TaskFunc_2(void *param)
-{
-  (void) param;
-
-  for (;;)
-  {
-    if (xSemaphoreTake(xSerialSemaphore, (TickType_t) 5) == pdTRUE)
-    {
-      Serial.println("Task_2");
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
-    
-      xSemaphoreGive(xSerialSemaphore);
-    }
-  }
-}
-
-void TaskFunc_3(void *param)
-{
-  (void) param;
-
-  for (;;)
-  {
-    if (xSemaphoreTake(xSerialSemaphore, (TickType_t) 5) == pdTRUE)
-    {
-      Serial.println("Task_3");
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
-    
-      xSemaphoreGive(xSerialSemaphore);
-    }
-  }
 }
---
+++ references/F053BTXIQFCEZ09.ino
@@ -0,0 +1,56 @@
+#include "DHT.h"
+#include <SPI.h>  
+#include "RF24.h"
+
+#define DHTPIN 4  
+#define DHTTYPE DHT22 
+
+RF24 myRadio (7, 8);
+byte addresses[][6] = {"0"};
+const int led_pin = 13;
+
+struct package
+{
+  float temperature ;
+  float humidity ;
+};
+
+
+typedef struct package Package;
+Package data;
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(led_pin, OUTPUT);
+    dht.begin();
+    myRadio.begin();  
+    myRadio.setChannel(115); 
+    myRadio.setPALevel(RF24_PA_MAX);
+    myRadio.setDataRate( RF24_250KBPS ) ; 
+    myRadio.openWritingPipe( addresses[0]);
+    delay(1000);
+}
+
+
+
+void loop()
+{
+  digitalWrite(led_pin, HIGH); // Flash a light to show transmitting
+  readSensor();
+  Serial.println(data.humidity);
+  Serial.println(data.temperature);
+  myRadio.write(&data, sizeof(data)); 
+  digitalWrite(led_pin, LOW);
+  delay(1000);
+}
+
+void readSensor()
+{
+ data.humidity = dht.readHumidity();
+ data.temperature = dht.readTemperature();
+}
+
+
